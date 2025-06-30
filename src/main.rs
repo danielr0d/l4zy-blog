11852482560df7f198ba37630cc74e7d08868b3f
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-06-30 11:33:52 -0300

    main as entry point cfg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-    println("Bolsonaro é meu pastor");
+use std::net::TcpListener;
+
+use l4zy_blog::start;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::int();
+
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
+    start_blog(listener)?.await?;
+    Ok(())
 }
