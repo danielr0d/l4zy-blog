a50ad5d211b9f32fbb2086afd98285b600b37fc0
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-08-18 20:41:07 -0300

    fix: typo

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,12 @@ lazy_static{
 pub fn start_blog(listener: TcpListener) -> Result <Server, std::io::Error> {
     let server = HttpServer::new(move ||{
         App::new()
+            .app_data(web::Data::new(TEMPLATES.clone()))
             .wrap(middleware::Logger::default())
             .route("/health", web::get().to(HttpResponse::Ok))
+            .service(handlers::index)
     })
-    .listen(listerner)?
+    .listen(listener)?
     .run();
 
     Ok(srv)
