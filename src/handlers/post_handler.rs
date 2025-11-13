5c5b37e90c588af9cbe3cde1543f404900e1c2c2
danielr0d <daniel.rodrigues.98@hotmail.com>
2025-11-12 23:48:56 -0300

    feat: add routes to post handler

--- src/handlers/post_handler.rs
+++ src/handlers/post_handler.rs
@@ -1,5 +1,6 @@
 use std::{io::Error, fs};
-
+use actix_web::{web, get, Responder, HttpResponse};
+use pulldown_cmark::{Options, Parser, html};
 use super::home_handler::Frontmatter;
 
 fn extract_markdown(post_name:: &str) -> Result <String, Error> {
@@ -31,4 +32,51 @@ fn extract_frontmatter(post_name: &str) ->  Result<Frontmatter, Error> {
     };
 
     Ok(frontmatter)
+}
+
+#[get("/posts/{post_name}")]
+pub async fn post(
+    tmpl: web::Data<tera::Terra>,
+    post_name: web::Path<String>,
+    ) -> impl Responder {
+    let mut context = tera::Context::new();
+    let options = Options::empty();
+
+    let markdown_input = match extract_markdown(&post_name) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("{:?}", e);
+            return HttpResponse::NotFound()
+                .content_type("text/html")
+                .body("<p>Could not find this post - sorry! :( </p>")
+        }
+    };
+
+    let frontmatter = match extract_frontmatter(&post_name) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("{:?}", e);
+            return HttpResponse::NotFound()
+                .content_type("text/html")
+                .body("<p>Could not find this post - sorry! :( </p>")
+        }
+    };
+
+    let parser = Parser::new_ext(&markdown_input, options);
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    context.insert("post", &html_output);
+    context.insert("meta_data", &frontmatter);
+
+    match tmpl.render("post.html", &context) {
+        Ok(s) => HttpResponse::Ok().content_type("text/html").body(s),
+        Err(e) => {
+            println!("{:?}", e);
+            return HttpResponse::NotFound()
+                .content_type("text/html")
+                .body("<p>Could not find post - sorry!</p>")
+        }
+    }
 }
